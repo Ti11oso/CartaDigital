85587cce9c2b2cd000d57b8a166be192f2cc25e9
Ti11oso <deadpolitojunior@gmail.com>
2025-02-01 21:42:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("verVideo").addEventListener("click", function() {
+    let video = document.getElementById("miVideo");
+    video.style.display = "block"; // Muestra el video
+    video.play(); // Reproduce automáticamente
+});
